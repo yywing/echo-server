43ab96b765043afa4e14cc5625cc5d699562c609
yang.yu <yang.yu@chaitin.com>
2019-06-06 12:32:58 +0800

    feat: print format

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s", r)
+	log.Printf("[Method]: %s/n[RemoteAddr]: %s/n[REQUEST_URI]: %s/n[UserAgent]: %s/n[Cookies]: %s/n[Headers]: %s/n", r.Method, r.RemoteAddr, r.RequestURI, r.UserAgent(), r.Cookies(), r.Header)
 }
 
 func main() {
